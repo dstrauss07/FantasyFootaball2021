0c5538132c91f285dd71a97ea053bc88e27561b4
Dave Strauss <fmdenver@gmail.com>
2021-12-10 22:30:43 -0500

    creating library for fantasy football manager

---
+++ src/FantasyFootballLibrary/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyFootballLibrary
+{
+    public class Player
+    {
+        public int PlayerId { get; set;}
+        public int ContractYears { get; set;}
+        public int PlayerFirstName { get; set;}
+        public int PlayerLastName { get; set;}
+        public int PlayerAge { get; set;}
+        public int PlayerPosition { get; set;}
+        public int SleeperPlayerId { get; set;}
+        [ForeignKey("Team")]
+        public int TeamId { get; set;}
+        public Team Team { get; set;}
+    }
+}
---
+++ src/FantasyFootballLibrary/Team.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FantasyFootballLibrary
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set;}
+        public int TeamName { get; set;}
+        [ForeignKey("Owner")]
+        public int OwnerId { get; set;}
+        public Owner Owner { get; set;}
+        [ForeignKey("Contract")]
+        public int ContractId { get; set;}
+        public Contracts Contracts { get; set;}
+        [ForeignKey("Player")]
+        public int PlayerId { get; set; }
+        public Contracts Player { get; set; }
+    }
+}
